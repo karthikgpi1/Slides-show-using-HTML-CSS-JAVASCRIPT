a6498c8022182cad324e334b154fc8758c32e947
karthikgpi1 <karthikgopi024@gmail.com>
2022-07-10 02:49:46 -0700

    Auto slide

--- script.js
+++ script.js
@@ -33,7 +33,32 @@ function next(direction) {
         slides[i].classList.remove("active"); 
     }
     slides[index].classList.add("active");
+     
+
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace("active","");
+//     }
+//     dots[index -1].className += "active";   
+}
 
 
-    
+
+//-----------------------auto-----------
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.querySelector('.slider-items').children;
+    for(i=0; i<slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {
+        slideIndex = 1
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
 }
